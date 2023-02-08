7b797fbfde4bef39a544a3bed9a26880dd163a02
joanafm81 <114578901+joanafm81@users.noreply.github.com>
2023-02-08 09:37:44 +0000

    Fixed properties issue

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -9,8 +9,8 @@ export const LoginView = ({ onLoggedIn }) => {
     event.preventDefault();
 
     const data = {
-      acess: username,
-      secret: password
+      Username: username,
+      Password: password
     };
 
     fetch("https://myflix-jfm.herokuapp.com/login", {
